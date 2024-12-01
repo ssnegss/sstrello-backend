2992986b3ba0d0934d6d3340e9332464c98c478b
julia snegova <ssnnggvv@gmail.com>
2024-12-01 16:34:51 +0300

    feat(jwt): add jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from '../user/user.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	private configService;
+	private userService;
+
+	constructor(configService: ConfigService, userService: UserService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET'),
+		});
+	}
+
+	async validate({ id }: { id: string }) {
+		return this.userService.getById(id);
+	}
+}
