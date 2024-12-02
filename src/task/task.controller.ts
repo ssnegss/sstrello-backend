62ecfec645cc81a34debd07d8015bbfafa8ed38a
julia snegova <ssnnggvv@gmail.com>
2024-12-02 13:08:37 +0300

    feat(task): add endpoints

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,34 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	Delete,
+	HttpCode,
+	UsePipes,
+	ValidationPipe,
+	Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
 
-@Controller('task')
+@Controller('user/tasks')
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+	constructor(private readonly taskService: TaskService) {}
 
-  @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.taskService.create(createTaskDto);
-  }
+	@Post()
+	@HttpCode(200)
+	@Auth()
+	@UsePipes(new ValidationPipe())
+	async create(@Body() dto: CreateTaskDto, @CurrentUser('id') userId: string) {
+		return this.taskService.create(dto, userId);
+	}
 
-  @Get()
-  findAll() {
-    return this.taskService.findAll();
-  }
+	@Get()
+	@Auth()
+	async getAll(@CurrentUser('id') userId: string) {
+		return this.taskService.getAll(userId);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.taskService.findOne(+id);
-  }
+	@Put(':id')
+	@Auth()
+	@HttpCode(200)
+	@UsePipes(new ValidationPipe())
+	async update(
+		@Body() dto: UpdateTaskDto,
+		@CurrentUser('id') userId: string,
+		@Param('id') id: string,
+	) {
+		return this.taskService.update(dto, id, userId);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.taskService.update(+id, updateTaskDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.taskService.remove(+id);
-  }
+	@Delete(':id')
+	@Auth()
+	@HttpCode(200)
+	async delete(@Param('id') id: string) {
+		return this.taskService.delete(id);
+	}
 }
