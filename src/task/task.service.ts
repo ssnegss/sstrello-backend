ba20343de9c22203fb1292b50b81587b1ee3ec2f
julia snegova <ssnnggvv@gmail.com>
2024-12-02 11:55:30 +0300

    feat(task): add task

---
+++ src/task/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class TaskService {
+	constructor(private prisma: PrismaService) {}
+
+	getCompleted(id: string) {
+		return this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true,
+			},
+		});
+	}
+
+	create(createTaskDto: CreateTaskDto) {
+		return 'This action adds a new task';
+	}
+
+	findAll() {
+		return `This action returns all task`;
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} task`;
+	}
+
+	update(id: number, updateTaskDto: UpdateTaskDto) {
+		return `This action updates a #${id} task`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} task`;
+	}
+}
