bfd063853273e347e94a5eb695b6e090ed4df433
julia snegova <ssnnggvv@gmail.com>
2024-12-03 15:54:25 +0300

    feat(app): add app import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
-import { TimeBlockModule } from './time-block/time-block.module';
+import { TimerModule } from './timer/timer.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule, TimeBlockModule],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		TaskModule,
+		TimerModule,
+	],
 })
 export class AppModule {}
